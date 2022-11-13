05cd89eb637749a442dc30d656cd29ac666f59fc
mariandanila <danila96marian@gmail.com>
2022-11-13 23:20:58 +0200

    Modify almost done

--- lib/screens/DetailsScreen.dart
+++ lib/screens/DetailsScreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:grocery_app/models/GroceryModel.dart';
@@ -14,6 +15,15 @@ class DetailsScreenArguments {
   });
 }
 
+class ModifyItem {
+  final String name;
+  final double price;
+  ModifyItem({
+    required this.name,
+    required this.price,
+  });
+}
+
 class DetailsScreen extends StatefulWidget {
 //  final GroceryItem item;
   const DetailsScreen({super.key});
@@ -25,20 +35,45 @@ class DetailsScreen extends StatefulWidget {
 class _DetailsScreenState extends State<DetailsScreen> {
 //  final groceriesItem = TextEditingController();
 //  final groceriesPrice = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     final details = ModalRoute.of(context)!.settings.arguments as GroceryItem;
     final groceriesItem = TextEditingController(text: details.name);
     final groceriesPrice =
         TextEditingController(text: details.price.toString());
+    final itemToModify = ModifyItem(
+        name: groceriesItem.text, price: double.parse(groceriesPrice.text));
+
+    void deleteItem() {
+      Navigator.of(context).pop(itemToModify);
+    }
+
     return Scaffold(
       appBar: AppBar(
-        title: Text('Edit item'),
-        centerTitle: true,
-        leading: const BackButton(
-          color: Colors.black,
-        ),
-      ),
+          title: Text('Edit item'),
+          centerTitle: true,
+          leading: const BackButton(
+            color: Colors.black,
+          ),
+//        leading: Builder(builder: (BuildContext context) {
+//          return IconButton(onPressed: onPressed, icon: icon)
+//        }
+          actions: [
+            Container(
+                width: 30,
+                height: 30,
+                color: Colors.red,
+                child: IconButton(
+                  icon: Icon(
+                    Icons.save,
+                    color: Colors.white,
+                  ),
+                  onPressed: () {
+                    deleteItem();
+                  },
+                ))
+          ]),
       body: Column(children: [
         TextFormField(
           controller: groceriesItem,
